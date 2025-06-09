5a6e8f449ccdb6931da5c26ce450322ef55fb235
leejonghyun93 <fhohffodf93@gmail.com>
2025-06-09 18:16:00 +0900

    1.초기셋팅 (1)

---
+++ src/main/java/org/kosa/shoppingmaillmanager/ShoppingmaillManagerApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.shoppingmaillmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmaillManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmaillManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kosa/shoppingmaillmanager/ShoppingmaillManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosa.shoppingmaillmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmaillManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
